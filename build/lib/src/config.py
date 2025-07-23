4136e41296cce91f538bb63162e7e2792b8da8e9
Mahesh <129616353+Mahesh8214@users.noreply.github.com>
2025-07-23 14:25:00 +0530

    Delete build/lib/src directory

--- build/lib/src/config.py
+++
@@ -1,7 +0,0 @@
-# src/config.py
-
-class Config:
-    RAW_DATA_PATH = "data/raw/Online Retail.xlsx"
-    MODEL_PATH = "saved_models/kmeans_model.pkl"
-    REF_DATE = "2011-12-10"
-    N_CLUSTERS = 4
